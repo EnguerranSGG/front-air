b87df53962c80730fc857db60c4bedf5ae50c13a
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-21 09:32:51 +0200

    feat(routes): add new routes and update header links for structures, statistics, steps, jobs, job offers, files, and news components

--- admin-dashboard/src/app/app.routes.ts
+++ admin-dashboard/src/app/app.routes.ts
@@ -4,6 +4,13 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HistoryComponent } from './pages/history/history.component';
 import { ValuesComponent } from './pages/values/values.component';
 import { AuthGuard } from './auth.guard';
+import { StructureComponent } from './pages/structures/structures.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { StepComponent } from './pages/steps/steps.component';
+import { JobComponent } from './pages/jobs/jobs.component';
+import { JobOfferComponent } from './pages/job-offers/job-offers.component';
+import { FileComponent } from './pages/files/files.component';
+import { NewsComponent } from './pages/news/news.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -11,5 +18,12 @@ export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'history', component: HistoryComponent, canActivate: [AuthGuard] },
   { path: 'values', component: ValuesComponent, canActivate: [AuthGuard] },
+  { path: 'structures', component: StructureComponent, canActivate: [AuthGuard] },
+  { path: 'statistics', component: StatisticsComponent, canActivate: [AuthGuard] },
+  { path: 'steps', component: StepComponent, canActivate: [AuthGuard] },
+  { path: 'jobs', component: JobComponent, canActivate: [AuthGuard] },
+  { path: 'files', component: FileComponent, canActivate: [AuthGuard] },
+  { path: 'job-offers', component: JobOfferComponent, canActivate: [AuthGuard] },
+  { path: 'news', component: NewsComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
