fcf8e4535c83ec581ee77678e1f41f6901a4574f
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-21 09:31:48 +0200

    feat(files): add FileService for managing file operations with CRUD functionality and download support

---
+++ admin-dashboard/src/app/services/file.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FileEntity } from '../models/file.model';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+  private apiUrl = `${environment.API_URL}/files`;
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<FileEntity[]> {
+    return this.http.get<FileEntity[]>(`${this.apiUrl}/all`);
+  }
+
+  create(formData: FormData): Observable<FileEntity> {
+    return this.http.post<FileEntity>(`${this.apiUrl}/upload`, formData);
+  }
+
+  update(id: number, formData: FormData): Observable<FileEntity> {
+    return this.http.put<FileEntity>(`${this.apiUrl}/${id}`, formData);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  getById(id: number): Observable<FileEntity> {
+    return this.http.get<FileEntity>(`${this.apiUrl}/${id}`);
+  }
+
+  download(id: number): Observable<Blob> {
+    return this.http.get(`${this.apiUrl}/${id}/download`, {
+      responseType: 'blob',
+    });
+  }
+  
+}
