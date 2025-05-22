9bc234a43058d3f8095bfa35d1c11568d5b93b5c
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-23 00:11:33 +0200

    feat(admin): add styles for login and main layout, removing dashboard route for streamlined navigation

--- admin-dashboard/src/app/app.routes.ts
+++ admin-dashboard/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HistoryComponent } from './pages/history/history.component';
 import { ValuesComponent } from './pages/values/values.component';
 import { AuthGuard } from './auth.guard';
@@ -15,7 +14,6 @@ import { NewsComponent } from './pages/news/news.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'history', component: HistoryComponent, canActivate: [AuthGuard] },
   { path: 'values', component: ValuesComponent, canActivate: [AuthGuard] },
   { path: 'structures', component: StructureComponent, canActivate: [AuthGuard] },
