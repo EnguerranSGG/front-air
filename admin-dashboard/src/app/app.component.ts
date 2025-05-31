84fcab7fc746a921c383f51d856387a5b5448a40
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-31 15:07:45 +0200

    feat(admin): add error message styles and update routing for dashboard component

--- admin-dashboard/src/app/app.component.ts
+++ admin-dashboard/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './pages/header/header.component';
 import { CommonModule } from '@angular/common';
 import { ToastComponent } from './utils/toast/toast.component';
 import { ToastService } from './utils/toast/toast.service';
--- admin-dashboard/src/app/app.routes.ts
+++ admin-dashboard/src/app/app.routes.ts
@@ -6,10 +6,9 @@ import { AuthGuard } from './auth.guard';
 import { StructureComponent } from './pages/structures/structures.component';
 import { StatisticsComponent } from './pages/statistics/statistics.component';
 import { StepComponent } from './pages/steps/steps.component';
-import { JobComponent } from './pages/jobs/jobs.component';
 import { JobOfferComponent } from './pages/job-offers/job-offers.component';
 import { FileComponent } from './pages/files/files.component';
-import { NewsComponent } from './pages/news/news.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -19,9 +18,8 @@ export const routes: Routes = [
   { path: 'structures', component: StructureComponent, canActivate: [AuthGuard] },
   { path: 'statistics', component: StatisticsComponent, canActivate: [AuthGuard] },
   { path: 'steps', component: StepComponent, canActivate: [AuthGuard] },
-  { path: 'jobs', component: JobComponent, canActivate: [AuthGuard] },
   { path: 'files', component: FileComponent, canActivate: [AuthGuard] },
   { path: 'job-offers', component: JobOfferComponent, canActivate: [AuthGuard] },
-  { path: 'news', component: NewsComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
