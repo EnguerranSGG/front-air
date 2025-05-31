1664c19d3578f8a46533a74b023dc3e268bca073
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-31 15:00:50 +0200

    refactor(admin): remove job and position models, job component and associated services

--- admin-dashboard/src/app/models/job.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Job {
-    job_id: number;
-    function: string;
-    position_id: number;
-  }
-  
--- admin-dashboard/src/app/models/position.model.ts
+++
@@ -1,5 +0,0 @@
-export interface Position {
-    position_id: number;
-    name: string;
-  }
-  
