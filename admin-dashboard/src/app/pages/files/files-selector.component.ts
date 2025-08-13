ec858cb39c6a2237a2e2b6039cea4da107052e7a
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-08-13 11:01:54 +0200

    feat(admin): implement filtering for protected files in file selector

--- admin-dashboard/src/app/pages/files/files-selector.component.ts
+++ admin-dashboard/src/app/pages/files/files-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, computed } from '@angular/core';
 import { FileEntity } from '../../models/file.model';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -17,6 +17,14 @@ export class FileSelectorComponent {
   @Input() files: FileEntity[] = [];
   @Input() selectedFileId: number | null = null;
   @Output() fileSelected = new EventEmitter<number>();
+
+  // Filtrage des fichiers protégés (même logique que dans files.component.ts)
+  private readonly PROTECTED_FILE_IDS = [8, 11, 16, 17, 18, 26, 27, 28, 29, 30];
+  
+  // Propriété computed qui filtre automatiquement les fichiers protégés
+  filteredFiles = computed(() => {
+    return this.files.filter(file => !this.PROTECTED_FILE_IDS.includes(file.file_id));
+  });
 
   onSelect(id: number): void {
     this.fileSelected.emit(id);
